6a7b2bcc1b8d7a1c859124fee092608dbf6ee33d
Van Viet <vietqv@lsd.>
2025-11-16 00:52:13 +0700

    add grid config entity and salary composition system entity

---
+++ MISA.Salary.Domain/Entitites/GridConfig.cs
@@ -0,0 +1,43 @@
+﻿using MISA.Salary.Domain.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MISA.Salary.Domain.Entitites;
+
+[Table("pa_grid_config")]
+public class GridConfig : IEntity<int>
+{
+    [Key]
+    [Column("grid_config_id")]
+    public int Id { get; set; }
+
+    [Column("sub_system_code")]
+    public string SubSystemCode { get; set; } = string.Empty;
+
+    [Column("field_name")]
+    public string FieldName { get; set; } = string.Empty;
+
+    [Column("caption")]
+    public string Caption { get; set; } = string.Empty;
+
+    [Column("tooltip")]
+    public string Tooltip { get; set; } = string.Empty;
+
+    [Column("data_type")]
+    public string DataType { get; set; } = string.Empty;
+
+    [Column("is_visible")]
+    public bool IsVisible { get; set; }
+
+    [Column("is_lock")]
+    public bool IsLock { get; set; }
+
+    [Column("is_fixed")]
+    public bool IsFixed { get; set; }
+    
+    [Column("is_default")]
+    public bool IsDefault { get; set; }
+
+    [Column("sort_order")]
+    public int SortOrder { get; set; }
+}
---
+++ MISA.Salary.Domain/Entitites/SalaryCompositionSystem.cs
@@ -0,0 +1,57 @@
+﻿using MISA.Salary.Domain.Abstract;
+using MISA.Salary.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ValueType = MISA.Salary.Domain.Enums.ValueType;
+
+namespace MISA.Salary.Domain.Entitites;
+
+[Table("pa_salary_composition_system")]
+public class SalaryCompositionSystem : IEntity<int>
+{
+    [Key]
+    [Column("salary_composition_system_id")]
+    public int Id { get; set; }
+
+    [Column("salary_composition_system_name")]
+    public string SalaryCompositionName { get; set; } = string.Empty;
+
+    [Column("salary_composition_system_code")]
+    public string SalaryCompositionCode { get; set; } = string.Empty;
+
+    [Column("salary_composition_system_type")]
+    public CompositionType? CompositionType { get; set; }
+
+    [Column("salary_composition_system_nature")]
+    public CompositionNature? CompositionNature { get; set; }
+
+    [Column("salary_composition_system_taxable")]
+    public bool? Taxable { get; set; }
+
+    [Column("salary_composition_system_tax_deduction")]
+    public bool? TaxDeduction { get; set; }
+
+    [Column("salary_composition_system_quota")]
+    public string? QuotaFormula { get; set; }
+
+    [Column("salary_composition_system_formula")]
+    public string? Formula { get; set; }
+
+    [Column("salary_composition_system_value_type")]
+    public ValueType ValueType { get; set; }
+
+    [Column("salary_composition_system_description")]
+    public string? Description { get; set; }
+
+    [Column("salary_composition_system_status")]
+    public Status Status { get; set; }
+
+    [Column("salary_composition_system_option_show_paycheck")]
+    public OptionShowPaycheck? OptionShowPaycheck { get; set; }
+
+    [Column("salary_composition_system_is_not_allow_delete")]
+    public bool IsNotAllowDelete { get; set; }
+
+    [Column("salary_composition_system_is_default")]
+    public bool IsDefault { get; set; }
+}
